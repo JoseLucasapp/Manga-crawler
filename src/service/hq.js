7f6205ad09f63aef246f2c7127a03889ebc86649
José Lucas <josluc2016@gmail.com>
2023-03-02 02:42:40 -0300

    feat: get pdf link

--- src/service/hq.js
+++ src/service/hq.js
@@ -1,6 +1,6 @@
 import puppeteer from 'puppeteer'
 
-const url = 'https://hqonlinegratis.com.br/'
+const url = 'https://hqonlinegratis.com.br'
 
 const puppeteerFunction = async (query) => {
     const browser = await puppeteer.launch({
@@ -12,12 +12,19 @@ const puppeteerFunction = async (query) => {
 
     const page = await browser.newPage()
 
-    await page.goto(url, { waitUntil: 'networkidle2' })
+    await page.goto(`${url}/page/1/`, { waitUntil: 'networkidle2' })
 
     await page.click('.nav-btn > label')
     await page.waitForSelector('input[name="s"]')
     await page.type('input[name="s"]', query.hq)
     await page.click('button[type="submit"]')
+
+    /*const pageInfo = await page.evaluate(() => {
+        const moreThanOnePage = document.getElementsByClassName('page-numbers').length > 0 ? true : false
+        const pages = document.getElementsByClassName('page-numbers').length
+
+        return { moreThanOnePage, pages }
+    })*/
 
     await page.waitForSelector('.hqs')
     const hqs = await page.$$eval("div.hqs > div.container-hqs > div.hq", (divs) => divs.map((hq) => {
@@ -27,6 +34,13 @@ const puppeteerFunction = async (query) => {
 
         return { url, img, name }
     }))
+
+    for (let i = 0; i < hqs.length; i++) {
+        await page.goto(hqs[i].url)
+        const pdf = await page.$$eval('.downloads', (as) => as.map((a) => a.querySelector('a').href))
+
+        hqs[i]["pdf"] = pdf[0]
+    }
 
     await browser.close()
 
